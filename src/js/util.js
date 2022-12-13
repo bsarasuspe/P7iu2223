ca3606a207ea56ddd7712e504cdc4561e441cc9f
Jaime <you@example.com>
2022-12-13 15:05:46 +0100

    fix me lo he inventao

--- src/js/util.js
+++ src/js/util.js
@@ -383,7 +383,7 @@ export function custom_confirm(msg) {
     return new Promise((r) => {
         confirmation_callback = (val) => {
             if (val == true) {
-                saveState(msg);
+                Cm.saveState(msg);
             }
             if (disable_next_confirmation) { disable_next_confirmation = false; return }
             document.querySelector("#confirmation_modal").className = "modal fade"
